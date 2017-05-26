ed80be79b4ffde4d0d85d0e83491bb5e6c7d9da1
Carisa Tong <ctong1124@gmail.com>
2017-05-26 13:00:53 -0400

    lesson 5

--- views/index.jsx
+++ views/index.jsx
@@ -13,14 +13,15 @@ import React from 'react';
     }
     
     class TodoList extends React.Component {
-       render() {
+        render() {
             return (
                 <div className="todoList">
                     <table style={{border: "2px solid black"}}>
-                        <tbody>
+                      <tbody>
                         <Todo title="Shopping">Milk</Todo>
                         <Todo title="Hair cut">13:00</Todo>
-                        </tbody>
+                        <Todo title="Learn React">15:00</Todo>
+                      </tbody>
                     </table>
                 </div>
             );
@@ -28,15 +29,20 @@ import React from 'react';
     }
 
    class Todo extends React.Component {
-      render() {
-        return(
-            <tr>
-                <td style={{border:"1px solid black"}}>{this.props.title}</td>
-                <td style={{border:"1px solid black"}}>{this.props.children}</td>
-            </tr>
-        );
-      }
+        render() {
+            return (
+                <tr>
+                    <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                    <td style={{border: "1px solid black"}}>{this.props.children}</td>
+                </tr>
+            );
+        }
     }
+
+    Todo.propTypes = {
+        title: React.PropTypes.string
+    };
+
     
     class TodoForm extends React.Component {
       render() {
